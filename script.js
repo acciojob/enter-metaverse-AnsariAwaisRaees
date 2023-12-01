779a83628b6d63abd74a46f6163d73c4811e05e8
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-12-02 00:06:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const p = document.getElementById("status")
+const btn = document.getElementById("enterBtn")
+const body = document.querySelector("body")
+function changeText(){
+    const h1 = document.createElement("h1")
+    h1.innerText = "Entered Metaverse"
+    p.innerHTML = ""
+	p.appendChild(h1)
+}
+btn.addEventListener("click", changeText)
